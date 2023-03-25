ac5cf0ee4e6fe5fc250e578cc7054b1ed7cebfc4
Diego Cornejo <diegof.cornejo@gmail.com>
2023-03-24 20:18:39 -0600

    Setup for Vercel

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+yum install wget
+
+wget -qO- https://micromamba.snakepit.net/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
+
+./bin/micromamba shell init -s bash -p ~/micromamba
+source ~/.bashrc
+
+# activate the environment and install a new version of Python
+micromamba activate
+micromamba install python=3.10 -c conda-forge -y
+
+# install the dependencies
+python -m pip install -r requirements-deploy.txt
+
+# build the JupyterLite site
+jupyter lite --version
+jupyter lite build --output-dir dist
